060fc68ff06a2458f3321b392339fc8e12f418c3
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-02-18 14:45:41 +0100

    Add a statement

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.production?
+
+unless AdminUser.exists?
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development? || Rails.env.production?
+end
